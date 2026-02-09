cecc1709d1cf7a63a7b75057c4df49108a7e811d
AJlMA3 <ajluch12@gmail.com>
2026-02-09 17:50:45 +0500

    Update utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -10,16 +10,15 @@ function isPathClear(fR, fC, tR, tC, boardState) {
     let currR = parseInt(fR) + dR;
     let currC = parseInt(fC) + dC;
 
-    // Идем строго до целевой клетки, не включая её
     while (currR !== parseInt(tR) || currC !== parseInt(tC)) {
-        // Проверка границ, чтобы не читать boardState[8] или [-1]
         if (currR < 0 || currR > 7 || currC < 0 || currC > 7) return false;
 
         if (boardState[currR][currC] !== '') {
-            return false; // Путь прерван фигурой
+            return false; 
         }
         currR += dR;
         currC += dC;
     }
     return true;
+
 }
